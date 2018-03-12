5da81d7785b818aaf3c2fd91f91455f65b9fa967
kirim <uthi1004@gmail.com>
2018-03-12 11:16:04 +0900

    contactsApp
    success

---
+++ contactsapp/src/components/AddContact.vue
@@ -0,0 +1,13 @@
+<template>
+	<contactForm mode="add" />
+</template>
+<script>
+	import ContactForm from './ContactForm.vue';
+    export default{
+        name:'add-contact',
+		components:{ContactForm}
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ contactsapp/src/components/UpdateContact.vue
@@ -0,0 +1,11 @@
+<template>
+	<contactForm mode="update" :contact="contact" />
+</template>
+<script>
+	import ContactForm from './ContactForm.vue';
+    export default{
+        name:'update-contact',
+		components:{ContactForm},
+		props:['contact']
+    }
+</script>
