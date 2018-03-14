d1ed3f02013d1dad8b8e11330182c7393f045ee2
kirim <uthi1004@gmail.com>
2018-03-14 10:51:11 +0900

    컨택앱서치
    액션공부2

---
+++ contactsapp_search/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <search placeholder="두글자 이상 입력 후 엔터!"></search>
+    <contact-list></contact-list>
+  </div>
+</template>
+
+<script>
+  import Search from './components/Search.vue';
+  import ContactList from './components/ContactList.vue';
+  export default {
+	  name: 'app',
+	  components:{Search,ContactList}
+  }
+</script>
+
+<style>
+
+</style>
---
+++ contactsapp_search/src/components/ContactList.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+		<table id="list">
+			<thead>
+			<tr>
+				<th>번호</th>
+				<th>이름</th>
+				<th>전화번호</th>
+				<th>주소</th>
+			</tr>
+			</thead>
+			<tbody id="contacts">
+			<tr v-show="contacts.status==='fail'">
+				<td colspan="4">{{contacts.message}}</td>
+			</tr>
+			<tr v-for="contact in contacts">
+				<td>{{contact.no}}</td>
+				<td>{{contact.name}}</td>
+				<td>{{contact.tel}}</td>
+				<td>{{contact.address}}</td>
+			</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<script>
+	import {mapState} from 'vuex';
+	export default {
+		name: "contact-list",
+		computed:mapState(['contacts'])
+	}
+</script>
+
+<style scoped>
+	#list{width:800px; font:13px "verdana"; border-collapse: collapse;}
+	#list tr{border-bottom:solid 1px black;}
+	#list thead tr{color:yellow; background-color:purple;}
+	#list td, #list th{text-align:center; vertical-align:middle; border:1px solid black;}
+</style>
