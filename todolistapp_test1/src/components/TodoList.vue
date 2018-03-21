f66592d383b4a7b6a0fea9a84bbd075f3d06a640
kirim <uthi1004@gmail.com>
2018-03-22 08:18:46 +0900

    todolistapp테스트

---
+++ todolistapp_test1/src/components/TodoList.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<div class="header">
+			<h2>Todo List</h2>
+			<input-todo></input-todo>
+		</div>
+		<list></list>
+	</div>
+</template>
+
+<script>
+	import InputTodo from "./InputTodo.vue";
+	import list from './List.vue';
+	export default {
+		name: "todo-list",
+		components:{InputTodo, list}
+	}
+</script>
+
+<style scoped>
+	*{box-sizing: border-box;}
+	.header{
+		background-color: purple; padding:30px 30px; color:yellow; text-align: center;
+	}
+	.header:after{
+		content:""; display: table; clear: both;
+	}
+</style>
